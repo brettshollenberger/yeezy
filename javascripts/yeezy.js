b270f949c9bd8e6c5c88f6a6c75669caadfbf4da
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-07-19 23:30:06 -0400

    Add initial specs

---
+++ javascripts/yeezy.js
@@ -0,0 +1,24 @@
+Object.prototype.expect = function(what) {
+  return what;
+};
+
+Object.prototype.toEq = function(what) {
+  return this == what;
+};
+
+Object.prototype.toNotEq = function(what) {
+  return this != what;
+};
+
+Object.prototype.toBeTrue = function() {
+  return this == true;
+};
+
+Object.prototype.toNotBeTrue = function() {
+  return this != true;
+};
+
+Object.prototype.toBeFalse = function() {
+  return this == false;
+};
+
