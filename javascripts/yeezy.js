bd467cc285dd3e0f5eece34582c2b280a6e7b512
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-07-20 00:06:46 -0400

    Add beWithin and toInclude

--- javascripts/yeezy.js
+++ javascripts/yeezy.js
@@ -30,3 +30,42 @@ Object.prototype.toBeLessThan = function(what) {
   return this < what;
 };
 
+Object.prototype.toBeGreaterThanOrEqualTo = function(what) {
+  return this >= what;
+};
+
+Object.prototype.toBeLessThanOrEqualTo = function(what) {
+  return this <= what;
+};
+
+function range(start, stop, step) {
+  if (stop == undefined) {
+    stop = 0;
+  };
+  if (step === undefined) {
+    step = 1;
+  };
+  if (start == undefined) {
+    throw "Needs at least one argument";
+  } else if (stop == 0) {
+    return_array = [];
+    for (i = 1; i <= start; i++) {
+      return_array.push(i);
+    };
+    return return_array
+  } else if (stop != 0) {
+    return_array = [];
+    for(i = start; i <= stop; i += step) {
+      return_array.push(i);
+    };
+    return return_array;
+  };
+};
+
+Object.prototype.toBeWithin = function(start, stop) {
+  return this >= start && this <= stop;
+};
+
+Object.prototype.toInclude = function(what) {
+  return what in this;
+};
